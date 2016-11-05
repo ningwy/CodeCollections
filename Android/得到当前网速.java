abfa9141ef8ddc65b821800d85b16422a0e73761
ningwy <ningwy@users.noreply.github.com>
2016-11-05 12:22:43 +0800

    Create 得到当前网速.java

---
+++ Android/得到当前网速.java
@@ -0,0 +1,22 @@
+public class Utils {
+
+    private long lastTotalRxBytes = 0;
+    private long lastTimeStamp = 0;
+
+    /**
+     * 得到当前的网络速度
+     * @return
+     */
+    public String getNetSpeed(Context context) {
+        String netSpeed;
+        long nowTotalRxBytes = TrafficStats.getUidRxBytes(context.getApplicationInfo().uid)==TrafficStats.UNSUPPORTED ? 0 :(TrafficStats.getTotalRxBytes()/1024);//转为KB;
+        long nowTimeStamp = System.currentTimeMillis();
+        long speed = ((nowTotalRxBytes - lastTotalRxBytes) * 1000 / (nowTimeStamp - lastTimeStamp));//毫秒转换
+
+        lastTimeStamp = nowTimeStamp;
+        lastTotalRxBytes = nowTotalRxBytes;
+        netSpeed  = String.valueOf(speed) + " kb/s";
+        return  netSpeed;
+    }
+
+}
